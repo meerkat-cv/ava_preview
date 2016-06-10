152886bf2d82e9ca26110a3763c386e53fa4ccd2
Gustavo Führ <gustavofuhr87@gmail.com>
2016-06-09 21:06:08 -0300

    New button to flip cameras

--- app/src/main/java/br/com/meerkat/avapreview/MainActivity.java
+++ app/src/main/java/br/com/meerkat/avapreview/MainActivity.java
@@ -44,10 +44,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         // should request permission if android api > 23
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             boolean hasWritePermission = (ContextCompat.checkSelfPermission(this,
